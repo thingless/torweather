11330c75adc7b187033500eb3c5e179785a31937
FreeTheNation <rpklafter@yahoo.com>
2016-07-09 18:12:19 -0700

    more work

--- torweather.py
+++ torweather.py
@@ -2,6 +2,9 @@ import sqlite3
 import requests
 import dateutil.parser
 import json
+from time import mktime
+from datetime import datetime
+import re
 
 
 #import requests
@@ -11,18 +14,61 @@ import json
 
 #dateutil.parser.parse("2016-07-06 09:00:00")
 
+def parse_time_str(time):
+    return dateutil.parser.parse(time)
+
+def to_timestamp(datetime):
+    return mktime(datetime.timetuple())
+
+def from_timestamp(timestamp):
+    return datetime.fromtimestamp(int(timestamp))
+
 def scrape_email(text):
-    
+    pass
 
 def main():
     #get json file
     with open('mock_data.json') as data_file:    
         data = json.load(data_file)
-    
-    #https://onionoo.torproject.org/details
+    #data = equests.get('https://onionoo.torproject.org/details').json()
     #connect and init db if not inited
     conn = sqlite3.connect('torweather.db')
     try:
-        conn.execute("SELECT * FROM nodes")
+        conn.execute("SELECT * FROM nodes;") #if there is not a nodes table this will fail
     except sqlite3.OperationalError:
-        c.execute('''CREATE TABLE nodes (fingerprint TEXT, last_seen DATETIME, email TEXT, first_seen DATETIME, consensus_weight real, contact TEXT, nickname TEXT)''')
+        conn.execute('''CREATE TABLE nodes (
+            fingerprint TEXT PRIMARY KEY,
+            last_seen INTEGER,
+            email TEXT,
+            first_seen INTEGER,
+            consensus_weight REAL,
+            contact TEXT,
+            nickname TEXT,
+            unsubscribed INTEGER DEFAULT 0);''')
+    #update or add new records
+    for node in data['relays']:
+        fingerprint = node['fingerprint']
+        email = re.search(r'[\w\.-]+@[\w\.-]+', node.get('contact',''))
+        email = email and email.group(0)
+        conn.execute('''
+            INSERT OR REPLACE INTO nodes 
+            (fingerprint, last_seen, email, first_seen, consensus_weight, contact, nickname, unsubscribed) values (
+                :fingerprint,
+                :last_seen,
+                :email,
+                :first_seen,
+                :consensus_weight,
+                :contact,
+                :nickname,
+                (select unsubscribed from nodes where fingerprint = :fingerprint)
+            );
+        ''', {
+            "fingerprint":fingerprint,
+            "last_seen":to_timestamp(parse_time_str(node['last_seen'])),
+            "email":email or None,
+            "first_seen":to_timestamp(parse_time_str(node['first_seen'])),
+            "consensus_weight":node.get('consensus_weight'),
+            "contact":node.get('contact'),
+            "nickname":node.get('nickname'),
+        })
+    #find nodes whos down/up state has changed
